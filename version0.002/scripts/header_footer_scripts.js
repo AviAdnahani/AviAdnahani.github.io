e1977ebfe7f55c6bba494e2475eda3592b1b4ed8
AviAdnahani <avrahamadnahani@gmail.com>
2020-04-24 16:10:38 +0300

    fix img position

--- version0.002/scripts/header_footer_scripts.js
+++ version0.002/scripts/header_footer_scripts.js
@@ -8,21 +8,40 @@ const FACTORE = 37
 var device_width = 0;
 var font_size = '';
 
-setElementsSize()
+setHeaderElementsSize()
 
 window.addEventListener("resize", function(event) {
-    display = document.getElementsByClassName('device_size')[0];
-    setElementsSize();
+    setHeaderElementsSize();
 })
 
-function setElementsSize() {
-    device_width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-    size = Math.ceil(device_width/FACTORE);
-    font_size = size < MAX_FONT_SIZE ? createSize(size): MAX_FONT_SIZE_STR;
-    font_size = size < MIN_FONT_SIZE ? MIN_FONT_SIZE_STR: font_size;
-    document.getElementById('header_bottom').style.fontSize = font_size;
+function getDeviceWidth() {
+    return (window.innerWidth > 0) ? window.innerWidth : screen.width;
+};
+
+function getDeviceHeight() {
+    return (window.innerHeight > 0) ? window.innerHeight : screen.height;
 };
 
 function createSize(num) {
     return num + 'px'
+};
+
+function getFontSize(width) {
+    font_size = width < MAX_FONT_SIZE ? createSize(width): MAX_FONT_SIZE_STR;
+    font_size = width < MIN_FONT_SIZE ? MIN_FONT_SIZE_STR: font_size;
+    return font_size
+}
+
+function setHeaderElementsSize() {
+    var device_width = getDeviceWidth()
+    var device_hieght = getDeviceHeight()
+    var edited_width = Math.ceil(device_width/FACTORE);
+    var edited_hieght = Math.ceil(device_hieght/FACTORE); // for logo and footer
+    
+    // set logo size
+    // some code
+    
+    // set font size
+    font_size = getFontSize(edited_width)
+    document.getElementById('header_bottom').style.fontSize = font_size;
 };
