63e2f884c18ffa2c3636718ca34fc29b85a71c2e
AviAdnahani <avrahamadnahani@gmail.com>
2020-05-05 13:33:32 +0300

    update links

--- scripts/header_footer_scripts.js
+++ scripts/header_footer_scripts.js
@@ -1,54 +1,26 @@
-const MAX_FONT_SIZE = 35
-const MAX_FONT_SIZE_STR = MAX_FONT_SIZE + 'px'
-const MIN_FONT_SIZE = 10
-const MIN_FONT_SIZE_STR = MIN_FONT_SIZE + 'px'
-const FACTORE = 37 
+// const MAX_FONT_SIZE = 35
+// const MAX_FONT_SIZE_STR = MAX_FONT_SIZE + 'px'
+// const MIN_FONT_SIZE = 10
+// const MIN_FONT_SIZE_STR = MIN_FONT_SIZE + 'px'
+// const FACTORE = 37 
 
 
-var device_width = 0;
-var font_size = '';
+// var device_width = 0;
+// var font_size = '';
 
-// setHeaderElementsSize();
 setMainElmentsMargin();
 stickyTopTable();
 
 //scroll
 window.addEventListener("scroll", function(event) {
-    stickyTopTable();
-});
-
-window.addEventListener("resize", function(event) {
-    setHeaderElementsSize();
     setMainElmentsMargin();
     stickyTopTable();
 });
 
-function getDeviceWidth() {
-    return (window.innerWidth > 0) ? window.innerWidth : screen.width;
-};
-
-function getDeviceHeight() {
-    return (window.innerHeight > 0) ? window.innerHeight : screen.height;
-};
-
-function createSize(num) {
-    return num + 'px'
-};
-
-function getFontSize(width) {
-    font_size = width < MAX_FONT_SIZE ? createSize(width): MAX_FONT_SIZE_STR;
-    font_size = width < MIN_FONT_SIZE ? MIN_FONT_SIZE_STR: font_size;
-    return font_size
-}
-
-function setHeaderElementsSize() {
-    var device_width = getDeviceWidth()
-    var device_hieght = getDeviceHeight()
-    var edited_width = Math.ceil(device_width/FACTORE);
-    var edited_hieght = Math.ceil(device_hieght/FACTORE); // for logo and footer
-    font_size = getFontSize(edited_width)
-    document.getElementById('header_bottom').style['fontSize'] = font_size;
-};
+window.addEventListener("resize", function(event) {
+     setMainElmentsMargin();
+    stickyTopTable();
+});
 
 function setMainElmentsMargin() {
     var height_header = window.getComputedStyle(header).getPropertyValue("height");
@@ -60,14 +32,8 @@ function setMainElmentsMargin() {
 }
 
 function stickyTopTable() {
-    var top = window.getComputedStyle(header).getPropertyValue("height");;
+    var top = window.getComputedStyle(header).getPropertyValue("height");
     for (const t of document.getElementsByTagName('th')) {
         t.style.top = top;
     }
-    
 }
-
-// function headerSwitch(turn_on) {
-//     document.getElementsByClassName('header_bottom_link').style.backgroundColor = "#006d77"; // off
-//     document.getElementById("").style.backgroundColor = "#83c5be";
-// }
